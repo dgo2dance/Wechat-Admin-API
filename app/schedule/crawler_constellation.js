6a45d2e4aa342d3f264f9444e5c434f1a2a283e2
zhengguihong <zhenggh@tututech.net>
2019-08-26 11:05:09 +0800

    修改爬虫抓取时间

--- app/schedule/crawler_constellation.js
+++ app/schedule/crawler_constellation.js
@@ -6,7 +6,7 @@ class CrawlerConstellation extends Subscription {
     // 通过 schedule 属性来设置定时任务的执行间隔等配置
     static get schedule() {
         return {
-            cron: '0 59 23 * * *', // 1 分钟间隔
+            cron: '0 01 0 * * *', // 1 分钟间隔
             type: 'all', // 指定所有的 worker 都需要执行
         }
     }
